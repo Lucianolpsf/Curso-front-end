839d95785083169b8c8eb1fc6003073422d8aeb7
bruno <kendy.iuata@gmail.com>
2023-09-04 17:35:48 -0300

    melhorando barra

--- js/script.js
+++ js/script.js
@@ -5,17 +5,39 @@ barraDeProcura.addEventListener('input', ()=> {
 
 
 function procura(valor) {
-    const titulos2 = document.querySelectorAll('.conteudo-card > h2')
+    const titulos2 = Array.from(document.querySelectorAll('h2'))
+    const containerSugestao = document.getElementById('ipesquisa-absolute')
+    titulos2.shift()
     const valorTratado = valor.toLowerCase()
+    console.log(titulos2[0])
     let indexes = []
+    while(containerSugestao.firstChild) {
+        containerSugestao.removeChild(containerSugestao.lastChild)
+    }
     for(i = 0; i < titulos2.length; i++) {
         console.log(comparacao(i, titulos2, valorTratado))
         if(comparacao(i, titulos2, valorTratado)) {
             indexes[indexes.length] = i
         }
+
     }
+    
+    if(indexes.length != 0) {
+        indexes.forEach(element => {
+            console.log(indexes[6], element)
+            if(indexes[6] != element) {
+                const sugestao = containerSugestao.appendChild(document.createElement('a')) 
+                sugestao.setAttribute('href', `#${titulos2[element].parentElement.id}` )
+                sugestao.classList.add('sugestao')
+                sugestao.innerText = titulos2[element].innerHTML
+                console.log(titulos2[element].innerHTML)
+            }
+        });
+    }
+
     console.log(indexes)
 }
+
 
 function comparacao(index, items, valor) {
     const titulo = items[index].innerText.toLowerCase()
