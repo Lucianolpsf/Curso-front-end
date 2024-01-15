039fe2f09a4c63a4d1de90e4ca4a91b6b4f07b56
Bruno <kendy.iuata@gmail.com>
2024-01-15 17:04:16 -0300

    bug do array.shift arrumado

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 const barraDeProcura = document.getElementById('ipesquisa')
 barraDeProcura.addEventListener('input', ()=> {
-    procura(barraDeProcura.value)
+    if (barraDeProcura.value.length > 3) {
+        procura(barraDeProcura.value)
+        
+    }
 })
-const titulos2 = Array.from(document.querySelectorAll('h2'))
+const titulos2 = [...(document.querySelectorAll('h2'))]
 
 function procura(valor) {
     console.log(titulos2)
     const containerSugestao = document.getElementById('ipesquisa-absolute')
-    titulos2.shift()
     const valorTratado = valor.toLowerCase()
     console.log(titulos2[0])
     let indexes = []
+
     while(containerSugestao.firstChild) {
         apagar(containerSugestao)
     }
@@ -25,12 +28,17 @@ function procura(valor) {
     if(indexes.length != 0) {
         let maximum = 0
         indexes.forEach(element => {
-            console.log(indexes[6], element)
+            console.log(indexes, element)
             if(maximum <= 5) {
                 const sugestao = containerSugestao.appendChild(document.createElement('a')) 
                 sugestao.setAttribute('href', `#${titulos2[element].parentElement.id}` )
                 sugestao.classList.add('sugestao')
                 sugestao.innerText = titulos2[element].innerHTML
+                sugestao.addEventListener('click', (evento) => {
+                    evento.stopImmediatePropagation()
+                    apagar(containerSugestao)
+                    barraDeProcura.value = ''
+                })
                 console.log(titulos2[element].innerHTML)
                 maximum++
             }
@@ -38,16 +46,16 @@ function procura(valor) {
     }
 
     if(valor == '') {
-        while(containerSugestao.firstChild) {
-            apagar(containerSugestao)
-        }
+        apagar(containerSugestao)
     }
     
     
 }
 
 function apagar(container) {
-    container.removeChild(container.lastChild)
+    while(container.firstChild) {
+        container.removeChild(container.lastChild)
+    }
 }
 
 function comparacao(index, items, valor) {
